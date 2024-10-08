2ea81fca50ef015d04caa761d42c65bfd62ae090
togueza <togueza@gmail.com>
2024-10-07 18:49:23 -0600

    Update TasksRepository.java
    fix path

--- reina-madre-app/reina-madre-backend/src/main/java/com.reina.madre/repository/TasksRepository.java
+++ reina-madre-app/reina-madre-backend/src/main/java/com.reina.madre/repository/TasksRepository.java
@@ -2,7 +2,7 @@ package com.reina.madre.repository;
 
 import java.util.List;
 
-import com.reina_madre.model.Tasks;
+import com.reina.madre.model.Tasks;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TasksRepository extends JpaRepository<Tasks, Long> {
